ea658bf2d04c3ff803d682e4cac1db6c34ac35ec
Caleb <liucalebjiale@gmail.com>
2026-07-21 10:08:21 -0400

    server implementation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from textwrap import dedent
 from time import time
 from uuid import uuid4
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 import blockchain as BC
 
@@ -17,13 +17,55 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = BC.Blockchain()
 
 
+
+
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new Block"
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" because its myself, lol
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
   
+
+
+
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "We'll add a new transaction"
+    values = request.get_json()
+
+    # Check for correct payload format
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
@@ -33,5 +75,17 @@ def full_chain():
     }
     return jsonify(response), 200
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
+'''
+Example Request Payload:
+
+{
+ "sender": "my address",
+ "recipient": "someone else's address",
+ "amount": 5
+}
+'''
